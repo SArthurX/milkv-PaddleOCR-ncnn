e08d76bfe9b92de12ad421c30fbef4a897237cbc
SArthurX <sxa05z@gmail.com>
2025-09-29 15:26:13 +0800

    patch: ncnn compilation results are stored in build/

--- tools/build_ncnn_risc.sh
+++ tools/build_ncnn_risc.sh
@@ -1,7 +1,8 @@
 project_root=$(pwd)
 ncnn_path="lib/ncnn"
-install_path=${project_root}/${ncnn_path}/ncnn_riscv
-build_dir=${project_root}/${ncnn_path}/build_riscv
+build_dir=${project_root}/${ncnn_path}/build
+install_path=${build_dir}/ncnn_riscv
+
 
 # 設置 RISCV_ROOT_PATH 環境變量
 export RISCV_ROOT_PATH="${project_root}/tools/toolchain/gcc/riscv64-linux-musl-x86_64"
@@ -11,7 +12,17 @@ mkdir -p ${build_dir}
 cd ${build_dir}
 
 cmake .. \
-  -DCMAKE_TOOLCHAIN_FILE=../toolchains/riscv64-unknown-linux-gnu.toolchain.cmake \
+  -DCMAKE_SYSTEM_NAME=Linux \
+  -DCMAKE_SYSTEM_PROCESSOR=riscv64 \
+  -DCMAKE_C_COMPILER="${RISCV_ROOT_PATH}/bin/riscv64-unknown-linux-musl-gcc" \
+  -DCMAKE_CXX_COMPILER="${RISCV_ROOT_PATH}/bin/riscv64-unknown-linux-musl-g++" \
+  -DCMAKE_FIND_ROOT_PATH="${RISCV_ROOT_PATH}/sysroot" \
+  -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER \
+  -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
+  -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
+  -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=ONLY \
+  -DCMAKE_C_FLAGS="-march=rv64gc" \
+  -DCMAKE_CXX_FLAGS="-march=rv64gc" \
   -DCMAKE_INSTALL_PREFIX=${install_path} \
   -DCMAKE_BUILD_TYPE=Release \
   -DNCNN_VULKAN=OFF \
